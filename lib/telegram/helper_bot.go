3fc94c28c084b656019f9617d8f3e83f6970e6cd
elmhuangyu <elmhuangyu@gmail.com>
2026-05-09 10:21:29 -0400

    feat: include chat and thread identifiers in helper bot message logs

--- lib/telegram/helper_bot.go
+++ lib/telegram/helper_bot.go
@@ -20,9 +20,16 @@ func StartHelper(ctx context.Context, token string) error {
 			return
 		}
 		if update.Message.Text != "" {
-			log.Info().Str("username", update.Message.From.Username).Int64("id", update.Message.From.ID).Msg("Receive message from")
+			log.Info().
+				Str("username", update.Message.From.Username).    // username
+				Int64("id", update.Message.From.ID).              // user id
+				Int64("chat_id", update.Message.Chat.ID).         // group id
+				Int("thread_id", update.Message.MessageThreadID). // topic id
+				Msg("Receive message from")
+
 			chatID := update.Message.Chat.ID
 			text := fmt.Sprintf("Your User ID: %d", update.Message.From.ID)
+
 			_, err := b.SendMessage(ctx, &bot.SendMessageParams{
 				ChatID:          chatID,
 				MessageThreadID: update.Message.MessageThreadID,
