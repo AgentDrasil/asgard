bfa34b9678a750d3df9e036df80c300edc55c5b0
elmhuangyu <elmhuangyu@gmail.com>
2026-08-04 23:56:38 -0400

    chore: update supported go version

--- cmd/aw/commands/agy.go
+++ cmd/aw/commands/agy.go
@@ -24,7 +24,7 @@ var (
 	agySession          string
 	agyUsage            bool
 	agyModel            string
-	supportedAgyVersion = "1.1.9"
+	supportedAgyVersion = "1.1.10"
 )
 
 var agyCmd = &cobra.Command{
