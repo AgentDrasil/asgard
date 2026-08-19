cf29e85e42d3755e35fd6e177f3fbb0957bd7dd0
elmhuangyu <elmhuangyu@gmail.com>
2026-08-18 21:19:53 -0400

    chore: bump supported AGY version to 1.1.14

--- cmd/aw/commands/agy.go
+++ cmd/aw/commands/agy.go
@@ -26,7 +26,7 @@ var (
 	agyUsage            bool
 	agyModel            string
 	agyAddTmpToDir      bool
-	supportedAgyVersion = "1.1.13"
+	supportedAgyVersion = "1.1.14"
 )
 
 var agyCmd = &cobra.Command{
