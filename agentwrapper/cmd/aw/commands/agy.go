518649b1e4bb29a483e57ee211df5cb56f906ea0
elmhuangyu <elmhuangyu@gmail.com>
2026-09-22 09:17:49 -0400

    chore: update agy version to 1.2.8

--- agentwrapper/cmd/aw/commands/agy.go
+++ agentwrapper/cmd/aw/commands/agy.go
@@ -27,7 +27,7 @@ var (
 	agyModel            string
 	agyAgent            string
 	agyAddTmpToDir      bool
-	supportedAgyVersion = "1.2.7"
+	supportedAgyVersion = "1.2.8"
 )
 
 var agyCmd = &cobra.Command{
